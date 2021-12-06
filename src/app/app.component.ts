7b0b1cca7b32a16fa289b092e4d2a3cd85fd2cfe
mark953fps <markville953@gmail.com>
2021-12-06 16:21:41 +0800

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,25 +12,25 @@ export class AppComponent implements OnInit {
   documentTypeList: any = [
     {},
     {
-      documentType: 'image/*'
+      documentType: 'image/*',
     },
     {
-      documentType: 'x-world/x-vrml'
-    }
+      documentType: 'x-world/x-vrml',
+    },
   ];
 
   public ngOnInit() {}
 
   public fileUploaded(event, index) {
     this.multipleSelectedFile[index] = event;
-    // console.log(this.multipleSelectedFile, 'fileupload');
-    this.fileTable.push({ documentName: event.files[0].name, documentType: this.selectedDocType['documentType'], file: event.files[0] });
-
-    // console.log(this.fileTable, 'this.fileTable')
-    // console.log(event.files[0], 'event name')
+    this.fileTable.push({
+      documentName: event.files[0].name,
+      documentType: this.selectedDocType['documentType'],
+      file: event.files[0],
+    });
   }
 
   public saveMultiFile() {
-    console.log(this.fileTable, 'this.fileTable')
+    console.log(this.fileTable, 'this.fileTable');
   }
 }
